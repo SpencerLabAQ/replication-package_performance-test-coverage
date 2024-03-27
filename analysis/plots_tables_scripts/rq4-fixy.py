8c8b6aafa4beee36e77e66ce5a6df4f3d6974a82
riccardoRubei <riccardo.rubei@gmail.com>
2024-03-27 18:13:42 +0100

    updates

---
+++ analysis/plots_tables_scripts/rq4-fixy.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy.stats import iqr
+import warnings
+import os
+warnings.filterwarnings(action='ignore', category=FutureWarning)
+
+data = pd.read_csv('../../inputs/Summary.csv', sep = ';', decimal=",")
+#JUNIT Average Execution Time
+
+data["Average_Time"] = data["Average_Time"].astype(float)
+
+data_sorted = data.sort_values('Project', ascending=True)
+
+sns.set(style="whitegrid")
+palette = 'colorblind'
+
+# Create the plot
+plt.figure(figsize=(6.4, 4.8))
+plot = sns.barplot(x="Project", y="Average_Time", data=data_sorted, palette=palette)
+plt.ylabel("Time in seconds")
+#plt.xlabel("Project Name")
+#plt.title("JUnit testing time")
+plt.xticks(rotation=60, ha='right', fontsize=12, rotation_mode='anchor')
+plt.ticklabel_format(style='plain', useOffset=False, axis='y')
+plt.axhline(y = 0.098,    # Line on y = 0.2
+           xmin = 0.0, # From the left
+           xmax = 1.0,
+           linestyle ="dashed") # To the right
+
+# Adjust bottom margin and save the plot as pdf
+plt.subplots_adjust(bottom=0.2)
+plt.yscale("log")
+plt.savefig('junit_average_time_log.pdf', format='pdf')
+
+plt.show()
